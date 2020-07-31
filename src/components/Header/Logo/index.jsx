d21199d198582cc164bf8dcf785e153e694c92ad
Vlad <vladekkrutek@gmail.com>
2020-07-31 19:06:19 +0300

    update imports

--- src/components/Header/Logo/index.jsx
+++ src/components/Header/Logo/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from "react-router-dom/modules/Link";
+import {Link} from "react-router-dom";
 import logo from 'src/assets/img/logo.png';
 
 const Logo = () => {
